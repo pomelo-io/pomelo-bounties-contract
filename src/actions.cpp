624102885a5c54c77dffc45609b5f09a0b8e2d07
YaroShkvorets <shkvorets@gmail.com>
2022-05-13 11:21:57 -0400

    fix handling max_rows=null

--- src/actions.cpp
+++ src/actions.cpp
@@ -111,7 +111,7 @@ void pomelo::setconfig( const optional<uint64_t> fee, const optional<name> login
 void pomelo::cleartable( const name table_name, const optional<name> scope, const optional<uint64_t> max_rows )
 {
     require_auth( get_self() );
-    const uint64_t rows_to_clear = *max_rows == 0 ? -1 : *max_rows;
+    const uint64_t rows_to_clear = (!max_rows || *max_rows == 0) ? -1 : *max_rows;
     const uint64_t value = scope ? scope->value : get_self().value;
 
     // tables
