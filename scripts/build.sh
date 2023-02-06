4a55f57129303d844b519e16962d21a567dc64c2
YaroShkvorets <shkvorets@gmail.com>
2023-02-06 18:12:06 -0500

    check the compiler in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,27 +5,40 @@ cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 
 # build
 
+if command -v blanc++ &> /dev/null
+then
+  CDT="blanc++"
+elif command -v cdt-cpp &> /dev/null
+then
+  CDT="cdt-cpp"
+else
+  echo "Error: neither blanc++ nor cdt-cpp was found on this system."
+  exit 1
+fi
+
+echo "Compiling with $CDT"
+
 echo "compiling... [work.pomelo]"
 if [[ $* == --debug ]]
 then
     echo "DEBUG mode enabled"
-    blanc++ work.pomelo.cpp -I include -D=DEBUG
+    $CDT work.pomelo.cpp -I include -D=DEBUG
 else
-    blanc++ work.pomelo.cpp -I include
+    $CDT work.pomelo.cpp -I include
 fi
 
 # additional builds
 if [ ! -f "./include/eosio.token/eosio.token.wasm" ]; then
     echo "compiling... [eosio.token]"
-    blanc++ ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm --no-missing-ricardian-clause
+    $CDT ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm --no-missing-ricardian-clause
 fi
 
 if [ ! -f "./include/eosn.login/login.eosn.wasm" ]; then
     echo "compiling... [login.eosn]"
-    blanc++ ./include/eosn.login/login.eosn.cpp -I include -o include/eosn.login/login.eosn.wasm --no-missing-ricardian-clause
+    $CDT ./include/eosn.login/login.eosn.cpp -I include -o include/eosn.login/login.eosn.wasm --no-missing-ricardian-clause
 fi
 
 if [ ! -f "./include/oracle.defi/oracle.defi.wasm" ]; then
     echo "compiling... [oracle.defi]"
-    blanc++ ./include/oracle.defi/oracle.defi.cpp -I include -o include/oracle.defi/oracle.defi.wasm --no-missing-ricardian-clause
+    $CDT ./include/oracle.defi/oracle.defi.cpp -I include -o include/oracle.defi/oracle.defi.wasm --no-missing-ricardian-clause
 fi
